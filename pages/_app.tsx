9d65982df6b6963ebf33e0d57f41ba37c4a53d3f
jethrocabaluna <jethrocabaluna310@gmail.com>
2022-10-27 23:56:21 +0800

    fix cors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,10 +22,16 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   )
 }
 
-const getBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_VERCEL_URL)
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return ''
+  }
 
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+
+  // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
 
@@ -35,12 +41,6 @@ export default withTRPC<AppRouter>({
     return {
       url,
       transformer: superjson,
-      fetch: (url, options) => {
-        return fetch(url, {
-          ...options,
-          credentials: 'include',
-        })
-      },
     }
   },
   ssr: true,
--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,35 +1,15 @@
 import * as trpcNext from '@trpc/server/adapters/next'
 import { appRouter } from 'server/routers/_app'
 import { createContext } from 'server/context'
-import { NextApiHandler } from 'next'
 
 export type AppRouter = typeof appRouter
 
-const allowCors =
-  (fn: NextApiHandler): NextApiHandler =>
-  async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin ?? '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext,
+  onError: ({ error }) => {
+    if (error.code === 'INTERNAL_SERVER_ERROR') {
+      console.error('Something went wrong', error)
     }
-    return await fn(req, res)
-  }
-
-export default allowCors(
-  trpcNext.createNextApiHandler({
-    router: appRouter,
-    createContext,
-    onError: ({ error }) => {
-      if (error.code === 'INTERNAL_SERVER_ERROR') {
-        console.error('Something went wrong', error)
-      }
-    },
-  })
-)
+  },
+})
